477ffeea9cb8ab29b6a4ceb6a217b7d703f2c537
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-05-30 10:51:50 -0500

    updated files

--- infra/aws_lambda.tf
+++ infra/aws_lambda.tf
@@ -24,7 +24,7 @@ resource "aws_lambda_function" "analysis_lambda" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda.lambda_handler"
 
-  source_code_hash = filebase64sha256("${path.root}/resources/lambda_function_payload.zip")
+  source_code_hash = filebase64sha256("${path.root}/resources/lambda_function_payload.py")
 
   runtime = "python3.8"
 
